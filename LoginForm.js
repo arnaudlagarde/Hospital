75dbd88ad970de7ade48befbebceab58dace7f81
NadimHipssi <121816205+NadimHipssi@users.noreply.github.com>
2023-06-21 14:48:45 +0200

    Page de connexion et d'inscription

---
+++ LoginForm.js
@@ -0,0 +1,97 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+import { View, TextInput, Button, Alert } from 'react-native';
+import { Collapse } from 'react-native-bootstrap'; // Veuillez noter que la bibliothèque 'react-native-bootstrap' doit être installée et configurée correctement
+
+const LoginForm = ({ setUserData, setIsAuthenticated }) => {
+  const [formData, setFormData] = useState({
+    email: '',
+    mot_de_passe: ''
+  });
+  const [error, setError] = useState('');
+  const [showMessage, setShowMessage] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const handleChange = (name, value) => {
+    setFormData({ ...formData, [name]: value });
+  };
+
+  const handleSubmit = async () => {
+    setIsLoading(true);
+
+    try {
+      const response = await axios.get('http://localhost:3000/users/login', {
+        params: {
+          email: formData.email,
+          mot_de_passe: formData.mot_de_passe
+        }
+      });
+
+      const user = response.data;
+      console.log(user);
+      if (user) {
+        const userData = {
+          nom: user.nom,
+          prenom: user.prenom,
+          admin: user.admin
+        };
+        setUserData(userData);
+        setIsAuthenticated(true);
+
+        if (userData.admin) {
+          // Naviguez vers la page souhaitée
+        }
+      } else {
+        setUserData(null);
+        setIsAuthenticated(false);
+        setShowMessage(true);
+        setTimeout(() => {
+          setShowMessage(false);
+        }, 3000); // Temps en millisecondes (3 secondes)
+      }
+    } catch (error) {
+      console.log(error);
+      setError("Une erreur s'est produite lors de l'ajout de l'utilisateur.");
+      setShowMessage(true);
+      setTimeout(() => {
+        setShowMessage(false);
+      }, 3000); // Temps en millisecondes (3 secondes)
+    }
+
+    setIsLoading(false);
+  };
+
+  if (isLoading) {
+    return <Text>Connexion en cours...</Text>;
+  }
+
+  return (
+    <View>
+      <TextInput
+        placeholder="Email"
+        value={formData.email}
+        onChangeText={text => handleChange('email', text)}
+        required
+      />
+      <TextInput
+        placeholder="Mot de passe"
+        secureTextEntry
+        value={formData.mot_de_passe}
+        onChangeText={text => handleChange('mot_de_passe', text)}
+        required
+      />
+      <Button title="Envoyer" onPress={handleSubmit} />
+      <Collapse in={showMessage}>
+        <View>
+          {error && (
+            <Alert variant='danger'>
+              {error}
+            </Alert>
+          )}
+        </View>
+      </Collapse>
+    </View>
+  );
+};
+
+export default LoginForm;
---
+++ RegistrationForm.js
@@ -0,0 +1,73 @@
+import React, { useState } from 'react';
+import { View, TextInput, Button, Alert, TouchableOpacity, Text } from 'react-native';
+
+const AccountPage = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+
+  const handleRegister = () => {
+    // Effectuez ici la logique d'inscription
+    if (password === confirmPassword) {
+      // Inscription réussie
+      Alert.alert('Inscription', 'Inscription réussie !');
+    } else {
+      // Les mots de passe ne correspondent pas
+      Alert.alert('Inscription', 'Les mots de passe ne correspondent pas !');
+    }
+  };
+
+  const handleLogin = () => {
+    // Effectuez ici la logique de connexion
+    // Validez l'e-mail et le mot de passe
+    if (email && password) {
+      // Connexion réussie
+      Alert.alert('Connexion', 'Connexion réussie !');
+    } else {
+      // Identifiants invalides
+      Alert.alert('Connexion', 'E-mail ou mot de passe invalide !');
+    }
+  };
+
+  return (
+    <View>
+      <TextInput
+        placeholder="E-mail"
+        onChangeText={text => setEmail(text)}
+        value={email}
+      />
+      <TextInput
+        placeholder="Mot de passe"
+        secureTextEntry
+        onChangeText={text => setPassword(text)}
+        value={password}
+      />
+      
+      {/* Champs supplémentaires pour l'inscription */}
+      <TextInput
+        placeholder="Confirmez le mot de passe"
+        secureTextEntry
+        onChangeText={text => setConfirmPassword(text)}
+        value={confirmPassword}
+      />
+
+      {/* Bouton d'inscription */}
+      <Button title="S'inscrire" onPress={handleRegister} />
+
+      {/* Déjà inscrit ? */}
+      <TouchableOpacity onPress={handleLogin}>
+        <Text>Déjà inscrit ? Connectez-vous ici.</Text>
+      </TouchableOpacity>
+
+      {/* Bouton de connexion */}
+      <Button title="Se connecter" onPress={handleLogin} />
+
+      {/* Mot de passe oublié */}
+      <TouchableOpacity>
+        <Text>Mot de passe oublié ? Cliquez ici.</Text>
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+export default AccountPage;
