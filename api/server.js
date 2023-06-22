59c38b9ee0e522af60c193442b0405dd461b8494
alagarde <xx-momo@hotmail.fr>
2023-06-22 10:05:14 +0200

    Update server.js

--- api/server.js
+++ api/server.js
@@ -31,6 +31,28 @@ const patientSchema = new mongoose.Schema({
 
 const Patients = mongoose.model('Patients', patientSchema);
 
+// Schema and model for doctors
+const doctorSchema = new mongoose.Schema({
+  role: String,
+  firstName: String,
+  lastName: String,
+  email: String,
+  password: String,
+});
+
+const Doctors = mongoose.model('Doctors', doctorSchema);
+
+// Schema and model for RHs
+const rhSchema = new mongoose.Schema({
+  role: String,
+  firstName: String,
+  lastName: String,
+  email: String,
+  password: String,
+});
+
+const RHs = mongoose.model('RHs', rhSchema);
+
 app.use(express.json());
 app.use(cors());
 
@@ -102,6 +124,154 @@ app.delete('/patients/:id', async (req, res) => {
       return res.status(404).json({ error: 'Patient not found' });
     }
     res.json(deletedPatient);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: 'Internal server error' });
+  }
+});
+
+// Create a doctor
+app.post('/doctors', async (req, res) => {
+  try {
+    const { role, firstName, lastName, email, password } = req.body;
+    const doctor = new Doctors({ role, firstName, lastName, email, password });
+    await doctor.save();
+    res.json(doctor);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: 'Internal server error' });
+  }
+});
+
+// Read all doctors
+app.get('/doctors', async (req, res) => {
+  try {
+    const doctors = await Doctors.find();
+    res.json(doctors);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: 'Internal server error' });
+  }
+});
+
+// Read a specific doctor
+app.get('/doctors/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+    const doctor = await Doctors.findById(id);
+    if (!doctor) {
+      return res.status(404).json({ error: 'Doctor not found' });
+    }
+    res.json(doctor);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: 'Internal server error' });
+  }
+});
+
+// Update a doctor
+app.put('/doctors/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { role, firstName, lastName, email, password } = req.body;
+    const updatedDoctor = await Doctors.findByIdAndUpdate(
+      id,
+      { role, firstName, lastName, email, password },
+      { new: true }
+    );
+    if (!updatedDoctor) {
+      return res.status(404).json({ error: 'Doctor not found' });
+    }
+    res.json(updatedDoctor);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: 'Internal server error' });
+  }
+});
+
+// Delete a doctor
+app.delete('/doctors/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+    const deletedDoctor = await Doctors.findByIdAndDelete(id);
+    if (!deletedDoctor) {
+      return res.status(404).json({ error: 'Doctor not found' });
+    }
+    res.json({ message: 'Doctor deleted successfully' });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: 'Internal server error' });
+  }
+});
+
+// Create an RH
+app.post('/rhs', async (req, res) => {
+  try {
+    const { role, firstName, lastName, email, password } = req.body;
+    const rh = new RHs({ role, firstName, lastName, email, password });
+    await rh.save();
+    res.json(rh);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: 'Internal server error' });
+  }
+});
+
+// Read all RHs
+app.get('/rhs', async (req, res) => {
+  try {
+    const rhs = await RHs.find();
+    res.json(rhs);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: 'Internal server error' });
+  }
+});
+
+// Read a specific RH
+app.get('/rhs/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+    const rh = await RHs.findById(id);
+    if (!rh) {
+      return res.status(404).json({ error: 'RH not found' });
+    }
+    res.json(rh);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: 'Internal server error' });
+  }
+});
+
+// Update an RH
+app.put('/rhs/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { role, firstName, lastName, email, password } = req.body;
+    const updatedRH = await RHs.findByIdAndUpdate(
+      id,
+      { role, firstName, lastName, email, password },
+      { new: true }
+    );
+    if (!updatedRH) {
+      return res.status(404).json({ error: 'RH not found' });
+    }
+    res.json(updatedRH);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: 'Internal server error' });
+  }
+});
+
+// Delete an RH
+app.delete('/rhs/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+    const deletedRH = await RHs.findByIdAndDelete(id);
+    if (!deletedRH) {
+      return res.status(404).json({ error: 'RH not found' });
+    }
+    res.json({ message: 'RH deleted successfully' });
   } catch (err) {
     console.error(err);
     res.status(500).json({ error: 'Internal server error' });
