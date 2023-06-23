f4173555ed641cf0b6696d5e4d88f966f7465543
alagarde <xx-momo@hotmail.fr>
2023-06-23 09:49:48 +0200

    Agenda improved :bulb:

--- components/DoctorAppointments.js
+++ components/DoctorAppointments.js
@@ -1,32 +1,76 @@
 import React, { useState, useEffect } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
+import { Agenda } from 'react-native-calendars';
 
 const DoctorAppointments = () => {
-  const [appointments, setAppointments] = useState([]);
+  const [appointments, setAppointments] = useState({});
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
-    // Ici, vous pouvez récupérer les rendez-vous à venir pour le médecin actuellement connecté depuis votre backend ou une source de données.
+    // Here, you can fetch upcoming appointments for the currently logged-in doctor from your backend or a data source.
 
-    // Exemple de données statiques pour illustrer l'affichage des rendez-vous
-    const dummyAppointments = [
-      { id: 1, patientName: 'John Doe', date: '2023-06-25', time: '10:00 AM' },
-      { id: 2, patientName: 'Jane Smith', date: '2023-06-26', time: '2:30 PM' },
-      // ... Ajoutez d'autres rendez-vous ici ...
-    ];
+    // Example static data to illustrate the appointment display
+    const dummyAppointments = {
+      '2023-06-25': [
+        { id: 1, patientName: 'John Doe', time: '10:00 AM' },
+        { id: 2, patientName: 'Jane Smith', time: '2:30 PM' },
+      ],
+      '2023-06-26': [
+        { id: 3, patientName: 'Alice Johnson', time: '9:00 AM' },
+        { id: 4, patientName: 'Bob Williams', time: '11:30 AM' },
+      ],
+      '2023-06-27': [
+        { id: 5, patientName: 'Emma Davis', time: '3:00 PM' },
+        { id: 6, patientName: 'Michael Brown', time: '4:30 PM' },
+      ],
+      '2023-06-28': [
+        { id: 7, patientName: 'Olivia Miller', time: '10:30 AM' },
+        { id: 8, patientName: 'William Wilson', time: '1:00 PM' },
+        { id: 9, patientName: 'Sophia Taylor', time: '3:30 PM' },
+      ],
+      // Add more appointments here...
+    };
 
     setAppointments(dummyAppointments);
+    setIsLoading(false);
   }, []);
+
+  const renderAppointment = (item) => (
+    <View style={styles.appointment}>
+      <Text style={styles.patientName}>{item.patientName}</Text>
+      <Text>Time: {item.time}</Text>
+    </View>
+  );
+
+  const renderEmptyDate = () => (
+    <View style={styles.emptyDate}>
+      <Text>No appointments</Text>
+    </View>
+  );
+
+  if (isLoading) {
+    return (
+      <View style={styles.container}>
+        <Text>Loading...</Text>
+      </View>
+    );
+  }
 
   return (
     <View style={styles.container}>
-      <Text style={styles.heading}>Rendez-vous à venir</Text>
-      {appointments.map(appointment => (
-        <View key={appointment.id} style={styles.appointment}>
-          <Text style={styles.patientName}>{appointment.patientName}</Text>
-          <Text>Date: {appointment.date}</Text>
-          <Text>Heure: {appointment.time}</Text>
-        </View>
-      ))}
+      <Text style={styles.heading}>Upcoming Appointments</Text>
+      <Agenda
+        items={appointments}
+        renderItem={renderAppointment}
+        renderEmptyDate={renderEmptyDate}
+        pastScrollRange={12} // Allow scrolling to past dates (number of months)
+        futureScrollRange={12} // Allow scrolling to future dates (number of months)
+        renderEmptyData={() => <View />} // Render an empty view when there are no items for a specific date
+        style={styles.agenda} // Apply styles to the Agenda component
+        theme={{
+          agendaKnobColor: 'gray', // Customize the color of the scroll knob
+        }}
+      />
     </View>
   );
 };
@@ -52,6 +96,16 @@ const styles = StyleSheet.create({
   patientName: {
     fontWeight: 'bold',
     marginBottom: 8,
+  },
+  emptyDate: {
+    height: 15,
+    flex: 1,
+    paddingTop: 30,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  agenda: {
+    flex: 1, // Make the Agenda component fill the entire screen height
   },
 });
 
