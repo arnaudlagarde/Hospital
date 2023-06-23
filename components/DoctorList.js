0fde3f400af3e898f7ae624aa895680c839662d7
alagarde <xx-momo@hotmail.fr>
2023-06-23 10:55:25 +0200

    Update DoctorList.js

--- components/DoctorList.js
+++ components/DoctorList.js
@@ -36,53 +36,54 @@ const DoctorList = () => {
     setSelectedDoctor((prevDoctor) => (prevDoctor?.email === doctor.email ? null : doctor));
   };
 
-  const handleEdit = (doctor) => {
+  const handleEditDoctor = (doctor) => {
     setEditedDoctor(doctor);
     setModalVisible(true);
   };
 
-  const handleSave = () => {
-    // Handle saving the edited doctor here
-    console.log('Save edited doctor:', editedDoctor);
-    setModalVisible(false);
+  const handleSave = async () => {
+    try {
+      await axios.put(`http://localhost:3000/doctors/${editedDoctor._id}`, editedDoctor);
+      setModalVisible(false);
+      fetchDoctors();
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const handleDeleteDoctor = async (id) => {
+    try {
+      await axios.delete(`http://localhost:3000/doctors/${id}`);
+      fetchDoctors();
+    } catch (error) {
+      console.error(error);
+    }
   };
 
   const renderDoctorItem = ({ item }) => {
     const isSelected = selectedDoctor && selectedDoctor.email === item.email;
-
-    const handleDelete = () => {
-      // Handle the delete functionality here
-      console.log('Delete doctor:', item);
-    };
-
+  
     return (
       <TouchableOpacity
         style={[styles.doctorItem, isSelected && styles.selectedDoctorItem]}
         onPress={() => handleDoctorPress(item)}
       >
-        <View style={styles.doctorHeader}>
-          <Text style={styles.doctorName}>{item.firstName} {item.lastName}</Text>
-          {isSelected && (
-            <View style={styles.actionsContainer}>
-              <TouchableOpacity style={styles.actionButton} onPress={() => handleEdit(item)}>
-                <Text style={styles.actionButtonText}>Edit</Text>
-              </TouchableOpacity>
-              <TouchableOpacity style={styles.actionButton} onPress={handleDelete}>
-                <Text style={styles.actionButtonText}>Delete</Text>
-              </TouchableOpacity>
-            </View>
-          )}
-        </View>
+        <Text style={styles.doctorName}>{item.firstName} {item.lastName}</Text>
         {isSelected && (
           <View style={styles.detailsContainer}>
             <Text style={styles.detailsText}>Email: {item.email}</Text>
-            <Text style={styles.detailsText}>Role: {item.role}</Text>
             {/* Add more details as needed */}
+            <View style={styles.buttonContainer}>
+              <Button title="Edit" onPress={() => handleEditDoctor(item)} />
+              <Button title="Delete" onPress={() => handleDeleteDoctor(item._id)} color="red" />
+              {/* Set color prop to "red" for the Delete button */}
+            </View>
           </View>
         )}
       </TouchableOpacity>
     );
   };
+  
 
   return (
     <View style={styles.container}>
@@ -94,12 +95,7 @@ const DoctorList = () => {
           keyExtractor={(item) => item.email}
         />
       </Transitioning.View>
-      <Modal
-        visible={modalVisible}
-        animationType="slide"
-        transparent={true}
-        onRequestClose={() => setModalVisible(false)}
-      >
+      <Modal visible={modalVisible} animationType="slide" onRequestClose={() => setModalVisible(false)}>
         <View style={styles.modalContainer}>
           <View style={styles.modalContent}>
             <Text style={styles.modalTitle}>Edit Doctor</Text>
@@ -115,15 +111,11 @@ const DoctorList = () => {
               value={editedDoctor?.lastName}
               onChangeText={(text) => setEditedDoctor({ ...editedDoctor, lastName: text })}
             />
-            <TextInput
-              style={styles.modalInput}
-              placeholder="Email"
-              value={editedDoctor?.email}
-              onChangeText={(text) => setEditedDoctor({ ...editedDoctor, email: text })}
-            />
-            {/* Add more input fields for other doctor details */}
-            <Button title="Save" onPress={handleSave} />
-            <Button title="Cancel" onPress={() => setModalVisible(false)} />
+            {/* Add more input fields for other properties */}
+            <View style={styles.modalButtonContainer}>
+              <Button title="Save" onPress={handleSave} />
+              <Button title="Cancel" onPress={() => setModalVisible(false)} />
+            </View>
           </View>
         </View>
       </Modal>
@@ -134,77 +126,68 @@ const DoctorList = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    padding: 16,
-    backgroundColor: '#f5f5f5',
+    padding: 20,
   },
   title: {
     fontSize: 24,
     fontWeight: 'bold',
-    marginBottom: 16,
+    marginBottom: 20,
   },
   doctorItem: {
-    backgroundColor: '#ffffff',
-    borderRadius: 8,
-    padding: 16,
-    marginBottom: 16,
+    marginBottom: 10,
+    padding: 10,
+    borderWidth: 1,
+    borderColor: '#ccc',
+    borderRadius: 5,
+    backgroundColor: '#fff',
   },
   selectedDoctorItem: {
-    marginBottom: 0,
-  },
-  doctorHeader: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignItems: 'center',
+    backgroundColor: '#eaf7ff',
   },
   doctorName: {
     fontSize: 18,
     fontWeight: 'bold',
-    marginBottom: 8,
-  },
-  actionsContainer: {
-    flexDirection: 'row',
-  },
-  actionButton: {
-    backgroundColor: '#2980b9',
-    borderRadius: 8,
-    paddingVertical: 8,
-    paddingHorizontal: 12,
-    marginHorizontal: 4,
-  },
-  actionButtonText: {
-    color: '#ffffff',
-    fontSize: 16,
-    fontWeight: 'bold',
   },
   detailsContainer: {
-    marginTop: 8,
+    marginTop: 10,
   },
   detailsText: {
     fontSize: 16,
-    marginBottom: 4,
+  },
+  buttonContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    marginTop: 10,
   },
   modalContainer: {
     flex: 1,
-    backgroundColor: 'rgba(0, 0, 0, 0.5)',
     justifyContent: 'center',
     alignItems: 'center',
   },
   modalContent: {
-    backgroundColor: '#ffffff',
-    borderRadius: 8,
-    padding: 16,
-    margin: 32,
+    width: '80%',
+    padding: 20,
+    borderWidth: 1,
+    borderColor: '#ccc',
+    borderRadius: 5,
+    backgroundColor: '#fff',
   },
   modalTitle: {
     fontSize: 20,
     fontWeight: 'bold',
-    marginBottom: 16,
+    marginBottom: 20,
   },
   modalInput: {
-    backgroundColor: '#f5f5f5',
-    borderRadius: 4,
-    padding: 8,
-    marginBottom: 8,
+    marginBottom: 10,
+    padding: 10,
+    borderWidth: 1,
+    borderColor: '#ccc',
+    borderRadius: 5,
+  },
+  modalButtonContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    marginTop: 20,
   },
 });
 
